90bb5940f0bef6991030729d675527861121c91d
João Paulo <jpaulope@indracompany.com>
2021-08-09 11:40:05 -0300

    chore: rebuild solution

--- src/Infra.Data/Contexts/NewsContext.cs
+++ src/Infra.Data/Contexts/NewsContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace Infra.Data
 {
     public class NewsContext : DbContext
     {
+        public DbSet<News> News { get; set; }
+
         public NewsContext(DbContextOptions<NewsContext> options) : base(options) { }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder) { }
