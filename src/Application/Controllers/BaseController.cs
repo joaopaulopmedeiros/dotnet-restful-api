d9d76ef27861b62593e0e26030a7482e4b6110a5
João Paulo <jpaulope@indracompany.com>
2021-08-09 10:36:37 -0300

    refactor: remove code smells

--- src/Application/Controllers/BaseController.cs
+++ src/Application/Controllers/BaseController.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Presentation.Commom.Messages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Application.Controllers
 {
